8ab892b68fb0738db3ea57fdb8a214b5f6f34a48
machiza <machizaarmando@gmail.com>
2020-08-25 10:26:39 +0200

    Validando formulário de login com Vuelidate

--- src/modules/auth/views/Login.vue
+++ src/modules/auth/views/Login.vue
@@ -28,26 +28,31 @@
                 name="email"
                 label="Email"
                 type="email"
+                v-model.trim="$v.user.email.$model"
               ></v-text-field>
               <v-text-field
                 prepend-icon="lock"
                 name="password"
                 label="Senha"
                 type="password"
+                v-model.trim="$v.user.password.$model"
               ></v-text-field>
             </v-form>
             <v-btn
               block
               depressed
               color="secondary"
+              @click="log"
             >Criar Conta</v-btn>
           </v-card-text>
 
           <v-card-actions>
             <v-spacer></v-spacer>
             <v-btn
+              :disabled="$v.$invalid"
               color="primary"
               large
+              @click="submit"
             >Login</v-btn>
           </v-card-actions>
 
@@ -59,8 +64,37 @@
 </template>
 
 <script>
+
+import { required, email, minLength } from 'vuelidate/lib/validators'
+
 export default {
-  name: 'Login'
+  name: 'Login',
+  data: () => ({
+    user: {
+      email: '',
+      password: ''
+    }
+  }),
+  validations: {
+    user: {
+      email: {
+        required,
+        email
+      },
+      password: {
+        required,
+        minLength: minLength(6)
+      }
+    }
+  },
+  methods: {
+    log () {
+      console.log('Vuelidate: ', this.$v)
+    },
+    submit () {
+      console.log('User: ', this.user)
+    }
+  }
 }
 </script>
 
