3be4c83635c999988a80286898f0d94c845239ed
machiza <machizaarmando@gmail.com>
2020-08-27 12:31:20 +0200

    Component para Toolbar do Dashboard com Vuetify

---
+++ src/modules/dashboard/components/AppMenu.vue
@@ -0,0 +1,20 @@
+<template>
+
+</template>
+
+<script>
+export default {
+  name: 'AppMenu',
+  props: {
+    value: Boolean
+  },
+  data: () => ({
+    items: [
+      { title: 'Home', icon: 'dashboard', url: '/dashboard', exact: true }
+    ]
+  })
+}
+</script>
+
+<style>
+</style>
---
+++ src/modules/dashboard/components/AppToolbar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar
+    fixed
+    color="primary"
+  >
+    <v-app-bar-nav-icon @click.stop="$emit('hide', !show)"></v-app-bar-nav-icon>
+    <v-toolbar-title>Dashboard</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  name: 'AppToolbar',
+  props: {
+    show: Boolean
+  },
+  model: {
+    prop: 'show',
+    event: 'hide'
+  }
+}
+</script>
+
+<style>
+</style>
