7c595256c7829603d135ce0b011538a8daea705a
machiza <machizaarmando@gmail.com>
2020-09-02 15:28:01 +0200

    Listando Lançamentos de acordo com o mês selecionado

--- src/modules/dashboard/modules/finances/mixins/amount-color.js
+++ src/modules/dashboard/modules/finances/mixins/amount-color.js
@@ -3,7 +3,7 @@ export default {
     amountColor (amount) {
       return amount < 0
         ? 'error--text text--lighten-1'
-        : 'primary-text text--lighten-1'
+        : 'primary--text text--lighten-1'
     }
   }
 }
