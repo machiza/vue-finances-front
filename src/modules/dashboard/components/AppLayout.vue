9772247adef6bd31238658028403d55a0baebdc8
machiza <machizaarmando@gmail.com>
2020-09-03 09:37:59 +0200

    Floating Action Button: navegação adicional

--- src/modules/dashboard/components/AppLayout.vue
+++ src/modules/dashboard/components/AppLayout.vue
@@ -10,17 +10,22 @@
         </v-col>
       </v-row>
     </v-container>
+
+    <app-floating-button />
+
   </div>
 </template>
 
 <script>
 
+import AppFloatingButton from './AppFloatingButton.vue'
 import AppMenu from './AppMenu.vue'
 import AppToolbar from './AppToolbar.vue'
 
 export default {
   name: 'AppLayout',
   components: {
+    AppFloatingButton,
     AppMenu,
     AppToolbar
   },
