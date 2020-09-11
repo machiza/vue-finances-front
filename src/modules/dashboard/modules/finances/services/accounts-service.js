389143d23f55a4452998a057ab152a8197c7b3bd
machiza <machizaarmando@gmail.com>
2020-09-11 09:32:00 +0200

    AccountsService: Mutation para criar novas Contas e reescrita do Cache

--- src/modules/dashboard/modules/finances/services/accounts-service.js
+++ src/modules/dashboard/modules/finances/services/accounts-service.js
@@ -1,14 +1,40 @@
 import apollo from '@/plugins/apollo'
 
-import AccontsQuery from './../graphql/Accounts.gql'
+import AccountsQuery from './../graphql/Accounts.gql'
+import AccountCreateMutation from './../graphql/AccountCreate.gql'
 
 const accounts = async () => {
   const response = await apollo.query({
-    query: AccontsQuery
+    query: AccountsQuery
   })
   return response.data.accounts
 }
 
+const createAccount = async variables => {
+  const response = await apollo.mutate({
+    mutation: AccountCreateMutation,
+    variables,
+    update: (proxy, { data: { createAccount } }) => {
+      try {
+        const data = proxy.readQuery({
+          AccountsQuery
+        })
+
+        data.accounts = [...data.accounts, createAccount]
+
+        proxy.writeQuery({
+          query: AccountsQuery,
+          data
+        })
+      } catch (error) {
+        console.log('Query "accounts" has not been read yet!', error)
+      }
+    }
+  })
+  return response.data.createAccount
+}
+
 export default {
-  accounts
+  accounts,
+  createAccount
 }
