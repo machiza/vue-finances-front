62bd81103a598a6292c97b8bbe3b1fc1cfa95a06
machiza <machizaarmando@gmail.com>
2020-09-13 22:16:38 +0200

    RecordsFilters: model filters e binding no evento change

--- src/modules/dashboard/modules/finances/components/RecordsFilter.vue
+++ src/modules/dashboard/modules/finances/components/RecordsFilter.vue
@@ -55,6 +55,7 @@
                     :items="operations"
                     item-text="description"
                     item-value="value"
+                    @change="localFilters.type = $event"
                   ></v-select>
                 </v-list-item-subtitle>
               </v-list-item-content>
@@ -72,6 +73,7 @@
                     :items="accounts"
                     item-text="description"
                     item-value="id"
+                    @change="localFilters.accountsIds = $event"
                   ></v-select>
                 </v-list-item-subtitle>
               </v-list-item-content>
@@ -89,6 +91,7 @@
                     :items="categories"
                     item-text="description"
                     item-value="id"
+                    @change="localFilters.categoriesIds = $event"
                   ></v-select>
                 </v-list-item-subtitle>
               </v-list-item-content>
@@ -115,6 +118,11 @@ export default {
       { description: 'Receita', value: 'CREDIT' },
       { description: 'Despesa', value: 'DEBIT' }
     ],
+    localFilters: {
+      accountsIds: [],
+      categoriesIds: [],
+      type: undefined
+    },
     showFilterDialog: false,
     subscriptions: []
   }),
@@ -126,7 +134,7 @@ export default {
   },
   methods: {
     filter () {
-      console.log('Filters')
+      console.log('Filters: ', this.localFilters)
     },
     setItems () {
       this.subscriptions.push(
