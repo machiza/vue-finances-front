ce9c10b2702e82772929a6b1969c73045c962d86
machiza <machizaarmando@gmail.com>
2020-09-14 23:02:51 +0200

    ReportsHome: Exibindo ToolbarByMonth

--- src/modules/dashboard/modules/finances/views/ReportsHome.vue
+++ src/modules/dashboard/modules/finances/views/ReportsHome.vue
@@ -1,13 +1,25 @@
 <template>
-  <h1>Relatórios</h1>
+  <v-row class="d-flex flex-row flex-wrap">
+    <v-col cols="12">
+      <toolbar-by-month
+        format="MM-YYYY"
+        color="primary"
+      />
+    </v-col>
+  </v-row>
 </template>
 
 <script>
 
 import { mapActions } from 'vuex'
 
+import ToolbarByMonth from './../components/ToolbarByMonth'
+
 export default {
   name: 'ReportsHome',
+  components: {
+    ToolbarByMonth
+  },
   created () {
     this.setTitle('Relatórios')
   },
